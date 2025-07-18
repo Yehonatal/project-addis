85c7505a024950ca9e5f9518071209787a18e128
yehoantal <yonatanafewerk@gmail.com>
2025-07-18 23:15:23 +0300

    chore(backend setup): initial backend setup/config complete

---
+++ backend/src/types/types.ts
@@ -0,0 +1,11 @@
+export interface ISong {
+    id: string;
+    title: string;
+    artist: string;
+    album: string;
+    year: number;
+    genre: string;
+    duration: string;
+    createdAt: string;
+    updatedAt: string;
+}
