2982347a74df29013cb204578184c2ee38d0ca80
yehoantal <yonatanafewerk@gmail.com>
2025-07-19 14:45:14 +0300

    chore(frontend) : setup webpack, fixed dep issues, added prettier config, and ts config

--- frontend/webpack.config.ts
+++ frontend/webpack.config.ts
@@ -1,1 +1,113 @@
-// I need to learn how set this up
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import "webpack-dev-server";
+import path from "path";
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+const config = (env: any, argv: any) => {
+    const isProduction = argv.mode === "production";
+
+    return {
+        mode: isProduction ? "production" : "development",
+        entry: "./src/index.tsx",
+        output: {
+            path: path.resolve(__dirname, "dist"),
+            filename: isProduction ? "[name].[contenthash].js" : "[name].js",
+            clean: true,
+            publicPath: "/",
+        },
+        resolve: {
+            extensions: [".js", ".jsx", ".ts", ".tsx"],
+            alias: {
+                "@": path.resolve(__dirname, "src"),
+                "@components": path.resolve(__dirname, "src/components"),
+                "@componentStyles": path.resolve(
+                    __dirname,
+                    "src/components/styles"
+                ),
+                "@pages": path.resolve(__dirname, "src/pages"),
+                "@store": path.resolve(__dirname, "src/store"),
+                "@services": path.resolve(__dirname, "src/services"),
+                "@utils": path.resolve(__dirname, "src/utils"),
+                "@styles": path.resolve(__dirname, "src/styles"),
+            },
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx|ts|tsx)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader",
+                        options: {
+                            presets: [
+                                ["@babel/preset-env", { targets: "defaults" }],
+                                [
+                                    "@babel/preset-react",
+                                    { runtime: "automatic" },
+                                ],
+                                "@babel/preset-typescript",
+                            ],
+                        },
+                    },
+                },
+                {
+                    test: /\.css$/i,
+                    use: ["style-loader", "css-loader"],
+                },
+                {
+                    test: /\.(png|jpe?g|gif|svg)$/i,
+                    type: "asset/resource",
+                    generator: {
+                        filename: "images/[name].[hash][ext]",
+                    },
+                },
+                {
+                    test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                    type: "asset/resource",
+                    generator: {
+                        filename: "fonts/[name].[hash][ext]",
+                    },
+                },
+            ],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: "./public/index.html",
+                title: "Project Addis - Music List",
+                favicon: "./public/favicon.png",
+            }),
+        ],
+        devServer: {
+            static: {
+                directory: path.join(__dirname, "public"),
+            },
+            port: parseInt(process.env.PORT || "3000"),
+            open: true,
+            hot: true,
+            historyApiFallback: true,
+            proxy: [
+                {
+                    context: ["/api"],
+                    target: "http://localhost:5001",
+                    changeOrigin: true,
+                    secure: false,
+                },
+            ],
+        },
+        optimization: {
+            splitChunks: {
+                chunks: "all",
+                cacheGroups: {
+                    vendor: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name: "vendors",
+                        chunks: "all",
+                    },
+                },
+            },
+        },
+        devtool: isProduction ? "source-map" : "eval-source-map",
+    };
+};
+export default config;
