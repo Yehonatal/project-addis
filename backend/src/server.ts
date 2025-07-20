7061fe5e80ae7685dd5b767cec99cf8a61c7d0cd
yehoantal <yonatanafewerk@gmail.com>
2025-07-20 13:57:01 +0300

    chore(backend build fix) : added "http://localhost:5000" the allowed origins

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,12 +8,19 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.set("trust proxy", 1);
+const allowedOrigins = ["http://localhost:3000", "http://localhost:5000"];
 
 // Basic middleware
 app.use(helmet());
 app.use(
     cors({
-        origin: "http://localhost:3000", // your frontend origin
+        origin: (origin, callback) => {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
         credentials: true,
     })
 );
