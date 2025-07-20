4f2e4d6bba2d57b15ef3d936b0c50e4c9a694b1d
yehoantal <yonatanafewerk@gmail.com>
2025-07-20 16:04:54 +0300

    chore(fix): added the backend and frontend urls to the allowed origins

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,7 +8,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.set("trust proxy", 1);
-const allowedOrigins = ["http://localhost:3000", "http://localhost:5000"];
+const allowedOrigins = [
+    "http://localhost:3000",
+    "http://localhost:5000",
+    "https://project-addis.vercel.app",
+    "https://project-addis.onrender.com",
+];
 
 // Basic middleware
 app.use(helmet());
--- frontend/src/services/songsAPI.ts
+++ frontend/src/services/songsAPI.ts
@@ -13,6 +13,7 @@ const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
 const api = axios.create({
     baseURL: API_URL,
     timeout: 10000,
+    withCredentials: true,
     headers: {
         "Content-Type": "application/json",
     },
